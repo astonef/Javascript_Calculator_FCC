bdb5bb857e87cdabbd1b6bede39b606a1b70fb15
mojo_pin <stefano.g.magni@proton.me>
2022-12-12 12:11:46 +0100

    main

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+
+  return (
+    <div className="container p-5">
+      <button className=" " id="bottone">1</button>
+      <button className="" id="bottone">2</button>
+      <button id="bottone">3</button>
+      <button id="bottone">4</button>
+      </div>
+  )
+}
+
+export default App
